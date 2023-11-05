1a7104700b825b1c19613e37371dd4dca0a9ca7e
l-ashish-gupta-l <ashish3072002@gmail.com>
2023-11-05 09:07:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import Header from './component/Header'
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
